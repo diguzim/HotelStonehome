a685ac6c437e0c314564d1fef440e9bc5020270c
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2017-06-11 15:33:19 -0300

    Criando o logo. Logo adicionado a barra de navegação no modo de tela grande, e está sendo omitido no modo de exibição de tela pequena.

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -1,6 +1,46 @@
 @extends ('layouts.master')
 
 @section ('content')
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+	<!-- Indicators -->
+	<ol class="carousel-indicators">
+		<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+		<li data-target="#myCarousel" data-slide-to="1"></li>
+		<li data-target="#myCarousel" data-slide-to="2"></li>
+		<li data-target="#myCarousel" data-slide-to="3"></li>
+	</ol>
+
+	<!-- Wrapper for slides -->
+	<div class="carousel-inner">
+		<div class="item active">
+			<img src="/images/hotel/pedra/01.jpg" alt="Entrada">
+		</div>
+
+		<div class="item">
+			<img src="/images/hotel/pedra/02.jpg" alt="Salão">
+		</div>
+
+		<div class="item">
+			<img src="/images/hotel/pedra/03.jpg" alt="Restaurante">
+		</div>
+
+		<div class="item">
+			<img src="/images/hotel/pedra/04.jpg" alt="Restaurante">
+		</div>
+	</div>
+
+	<!-- Left and right controls -->
+	<a class="left carousel-control" href="#myCarousel" data-slide="prev">
+		<span class="glyphicon glyphicon-chevron-left"></span>
+		<span class="sr-only">Previous</span>
+	</a>
+	<a class="right carousel-control" href="#myCarousel" data-slide="next">
+		<span class="glyphicon glyphicon-chevron-right"></span>
+		<span class="sr-only">Next</span>
+	</a>
+</div>
+<!--
 <div id="imagem">
 </div>
+-->
 @endsection
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,5 +1,6 @@
 <div class="container-fluid">
 	<ul class="nav nav-tabs">
+		<li class="navbar-brand"><a href="#"><img src="/images/hotel/pedra/logo/logo.png" alt="Dispute Bills"></a></li>
 		<li role="presentation"><a href="#">O Hotel</a></li>
 		<li role="presentation"><a href="#">Quartos</a></li>
 		<li role="presentation"><a href="#">Localização</a></li>
