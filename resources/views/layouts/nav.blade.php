def3af107518431e37a503a0d5ef974258ba82dc
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2017-06-11 22:02:44 -0300

    Feito a div de apresentação do hotel.

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,11 +1,23 @@
-<div class="container-fluid">
+<div class="container-fluid menu-topo-responsivo">
+	<a href="#"><img src="/images/hotel/pedra/logo/logo.png" alt="Dispute Bills"></a>
+</div>
+
+<div class="container-fluid menu-principal">
 	<ul class="nav nav-tabs">
 		<li class="navbar-brand"><a href="#"><img src="/images/hotel/pedra/logo/logo.png" alt="Dispute Bills"></a></li>
-		<li role="presentation"><a href="#">O Hotel</a></li>
-		<li role="presentation"><a href="#">Quartos</a></li>
-		<li role="presentation"><a href="#">Localização</a></li>
-		<li role="presentation"><a href="#">Galeria</a></li>
-		<li role="presentation"><a href="#">Contato</a></li>
-		<li role="presentation"><a href="#">Agende agora</a></li>
+		<li role="presentation"><a href="#">O HOTEL</a></li>
+		<li role="presentation"><a href="#">QUARTOS</a></li>
+		<li role="presentation"><a href="#">LOCALIZAÇÃO</a></li>
+		<li role="presentation"><a href="#">GALERIA</a></li>
+		<li role="presentation"><a href="#">CONTATO</a></li>
+		<li role="presentation"><a href="#">AGENDE AGORA</a></li>
 	</ul>
+</div>
+
+<div class="container-fluid" id="apresentacao">
+	<div id="texto">
+		<h1>Na belíssima região de Humber, onde pedra e madeira constroem memórias há mais de 700 anos</h1>
+		<p>Venha ao Hotel Stonehome e faça uma viagem pela história da Inglaterra moderna, onde conforto e tranquilidade reinam. O design clássico casado com o advento das revoluções tecnológicas possibilitaram a família Westminster a reconstruir o então destruído castelo Kensington, se tornando uma das maiores referências arquiterônicas contemporâneas. A decoração de seus salões e quartos, distribuídos em 3 andares, assim como os túneis subterrâneos e masmorras, possibilitam uma viagem a história da Europa medieval, enquanto o antiquário da era industrial nos possibilita compreender a transição para os tempos modernos, onde nossas piscinas aquecidas e quadras poliesportivas nos reforçam os confortos contemporâneos. </p>
+		<button type="button" class="btn btn-default btn-lg"><span>Conheça o hotel</span></button>
+	</div>
 </div>
